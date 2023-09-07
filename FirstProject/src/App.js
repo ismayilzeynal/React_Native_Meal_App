2851a40bcf9cf4f3bae8626e2fcf18148c08f687
ismayilzeynal <ismayilbz@code.edu.az>
2023-09-07 10:52:01 +0400

    fix: initial nav maked getStarted

--- FirstProject/src/App.js
+++ FirstProject/src/App.js
@@ -10,7 +10,7 @@ const Stack = createNativeStackNavigator();
 function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+            <Stack.Navigator initialRouteName="GetStarted" screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="GetStarted" component={GetStarted} />
                 <Stack.Screen name="Home" component={Home} />
                 <Stack.Screen
